6c87f83d3a37aa36f87fba9b1334903e84ddc222
skw <skwerlman@users.noreply.github.com>
2024-10-01 13:48:56 -0400

    remove specs which confused gradient

--- lib/native/crc24.ex
+++ lib/native/crc24.ex
@@ -3,7 +3,6 @@ defmodule Crc24.Native.Crc24 do
   use Rustler, otp_app: :crc24, crate: "crc24", features: ["nif"]
 
   @doc false
-  @spec crc24(any()) :: non_neg_integer()
   # accept any term, but raise argument error if we dont support getting the crc yet
   def crc24(data) when is_binary(data) do
     crc24_binary(data)
@@ -14,7 +13,6 @@ defmodule Crc24.Native.Crc24 do
   end
 
   @doc false
-  @spec crc24_binary(binary()) :: non_neg_integer()
   def crc24_binary(_data) do
     :erlang.nif_error(:nif_not_loaded)
   end
