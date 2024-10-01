6a01e3b7dd182ced6efb9d640f5d677bcfd2a151
skw <skwerlman@users.noreply.github.com>
2024-10-01 13:49:11 -0400

    comment native module

--- lib/native/crc24.ex
+++ lib/native/crc24.ex
@@ -1,5 +1,8 @@
 defmodule Crc24.Native.Crc24 do
+  # internal module to talk to the underlying rust nif
+
   @moduledoc false
+  # 'nif' feature is needed to tell rust to initialize the nif
   use Rustler, otp_app: :crc24, crate: "crc24", features: ["nif"]
 
   @doc false
@@ -13,6 +16,8 @@ defmodule Crc24.Native.Crc24 do
   end
 
   @doc false
+  # actually pass our request on the the nif
+  # this function is replaced when the nif is loaded
   def crc24_binary(_data) do
     :erlang.nif_error(:nif_not_loaded)
   end
