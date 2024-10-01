8bddde4e98aafb5f580659ccb901b92f50a48fe9
skw <skwerlman@users.noreply.github.com>
2024-10-01 13:47:04 -0400

    bump min dialyzer, update lockfile

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Crc24.MixProject do
       {:rustler, "~> 0.34", runtime: false},
       {:credo, "~> 1.7", runtime: false, only: [:dev, :test]},
       {:gradient, github: "esl/gradient", runtime: false, only: [:dev, :test]},
-      {:dialyxir, "~> 1.3", runtime: false, only: [:dev, :test]},
+      {:dialyxir, "~> 1.4", runtime: false, only: [:dev, :test]},
       {:stream_data, "~> 1.1", runtime: false, only: [:dev, :test]},
       {:ex_doc, ">= 0.0.0", runtime: false, only: :dev}
     ]
