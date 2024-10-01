2c6aefe39e783cf54a4c84f82605920426ba2f8f
skw <skwerlman@users.noreply.github.com>
2024-10-01 13:48:41 -0400

    raise on bad argument type

--- lib/native/crc24.ex
+++ lib/native/crc24.ex
@@ -4,11 +4,18 @@ defmodule Crc24.Native.Crc24 do
 
   @doc false
   @spec crc24(any()) :: non_neg_integer()
+  # accept any term, but raise argument error if we dont support getting the crc yet
   def crc24(data) when is_binary(data) do
     crc24_binary(data)
   end
 
+  def crc24(_data) do
+    raise ArgumentError
+  end
+
   @doc false
   @spec crc24_binary(binary()) :: non_neg_integer()
-  def crc24_binary(_data), do: :erlang.nif_error(:nif_not_loaded)
+  def crc24_binary(_data) do
+    :erlang.nif_error(:nif_not_loaded)
+  end
 end
