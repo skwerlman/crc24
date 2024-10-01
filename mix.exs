f7c58ad4ae70495103aed6543115904970414271
skw <skwerlman@users.noreply.github.com>
2024-10-01 16:46:51 -0400

    support and test versions back to OTP24+EX1.13

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Crc24.MixProject do
     [
       app: :crc24,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases()
